d597b01d9519e6fef1b19e98cdd303163f73b639
1alron <roma.niko393@gmail.com>
2024-09-20 14:27:05 +0300

    import added

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 import { useTelegram } from "../../hooks/useTelegram";
